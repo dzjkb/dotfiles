70c3c3b15c5629ac74ad661cf74e8f19c45718a7
dzjkb <jakub.pies@gmail.com>
2018-09-14 11:12:41 +0200

    install.sh: fixed saving old files and linking

--- install.sh
+++ install.sh
@@ -10,13 +10,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 ## install the dotfiles
 
 append_old() {
-    mv $1 $1.old
+    if [ -f $1 -o -d $1 ]; then
+        mv "$1" "$1.old"
+    elif [ -h $1 ]; then
+        rm $1
+    fi
 }
 
-for f in "vimrc zshrc"
+for f in vimrc zshrc
 do
-    local dotname=".$f"
-    append_old "~/$dotname"
+    dotname=".$f"
+    append_old ~/$dotname
     ln -s $ROOT/$f ~/$dotname
 done
 
